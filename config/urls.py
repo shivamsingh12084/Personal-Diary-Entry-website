4033fb2c0bd99c3572184b90c8b971534d9066ab
shivamsingh12084 <shivamsingh12084@gmail.com>
2021-12-23 17:45:56 +0530

    Deployment Ready

--- config/urls.py
+++ config/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     # Django admin
-    path('admin/', admin.site.urls),
+    path('anything-but-codingscholars/', admin.site.urls),
     # User Management
     path('accounts/', include('allauth.urls')),
     # Local apps
